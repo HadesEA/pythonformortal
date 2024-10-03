49e9ffed93dd84f2cbf993fe18531828e5ef5ba8
hades <adesea2209@gmail.com>
2024-10-03 14:20:41 -0700

    10 prácticas

---
+++ demo/home/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+
+class Tweet(models.Model):
+    content = models.CharField(max_length=256)
+    timestamp = models.DateTimeField(auto_now_add=True)
